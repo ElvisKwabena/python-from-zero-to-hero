bbaf9feb7fa6f8919aaa7135ee50364aa0067bd7
Elvis <eankrumah010@st.ug.edu.gh>
2022-11-01 12:11:53 +0000

    Modules and Packages

--- Modules_and_packages/myprogram.py
+++ Modules_and_packages/myprogram.py
@@ -1,3 +1,10 @@
-from mymodule import myfunc
-myfunc()
+#Importing from a module
+'''from mymodule import myfunc
+myfunc()'''
+#Importing from a package
+from MyMainPackage import some_main_script
+from MyMainPackage.SubPackage import mySubscript
 
+some_main_script.report_main()
+
+mySubscript.sub_report()
